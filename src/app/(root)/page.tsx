485d9d7a20934969923b972cef3ce81217982faa
Nisha0202 <nishajabatunnessa@gmail.com>
2024-11-01 13:47:59 +0600

    Hero color change

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <>
       <section className=" bg-custom">
-        <div className="heading text-gray-800">
+        <div className="heading text-white">
           Your Startup <span className="text-primary">Ideas</span> <br /> Our Community
         </div>
       </section>
